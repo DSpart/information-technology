f4fff8c019db9901456cccc1580d3e937af61358
Бакин Даниил <dan.bakin@outlook.com>
2021-02-28 15:19:32 +0300

    Добавьте файлы проекта.

---
+++ 3-d/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_d
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            double y = Convert.ToDouble(textBox2.Text);
+            double z = Convert.ToDouble(textBox3.Text);
+
+            textBox4.Text = "Результаты работы программы ст. Бакин Д.И. " +
+Environment.NewLine;
+            textBox4.Text += "При X = " + textBox1.Text + Environment.NewLine;
+            textBox4.Text += "При Y = " + textBox2.Text + Environment.NewLine;
+            textBox4.Text += "При Z = " + textBox3.Text + Environment.NewLine;
+
+            int n = 0;
+            if (radioButton2.Checked) n = 1;
+            else if (radioButton3.Checked) n = 2;
+
+            double u;
+            switch (n)
+            {
+                case 0:
+
+                   
+                    break;
+
+                case 1:
+
+                    
+                    break;
+
+                case 2:
+
+                    
+                    break;
+
+                default:
+                    textBox4.Text += "Решение не найдено" + Environment.NewLine;
+                    break;
+            }
+
+        }
+    }
+}
